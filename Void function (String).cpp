c32f1da11008619e1c0a82bce1d0f5d11244520d
RONIN <martinafful000@gmail.com>
2024-07-10 11:34:33 +0000

    Void function ( STRING)
    A void function is a non-returning value function.

---
+++ Void function (String).cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>//for string class
+#include<cmath> //The cmath library is used for mathematical operations .
+using namespace std;
+string Ninjaname ="Jax";// Input name of Rouge ninja here 
+//Void function specifies that the function doesn't return a value.
+void RougeNinjas(string s) {
+  cout << "My favorite rouge ninja is "<<s<< endl;
+}
+int main(){
+  
+ RougeNinjas(Ninjaname);
+  
+
+
+
+
+
+
+
+
+
+  return 0;
+}
+// RONIN COOKED THIS
