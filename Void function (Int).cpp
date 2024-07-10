acbd04893eb6a88222a21590fc1b4bbec79547dd
RONIN <martinafful000@gmail.com>
2024-07-10 11:44:23 +0000

    VOID FUNCTION (INTEGERS)

---
+++ Void function (Int).cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>//for string class
+#include<cmath> //The cmath library is used for mathematical operations .
+using namespace std;
+//Void function specifies that the function doesn't return a value.
+void RougeNinjas(int x) {
+  cout << "My future networth is $"<<x<< endl;
+}
+int main(){ 
+ RougeNinjas(100000000);
+    return 0;
+}
+// RONIN COOKED THIS
